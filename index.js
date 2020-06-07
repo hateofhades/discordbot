68348b8259d9a53500f538ca2951def646420f38
andreistalker <usadekall@gmail.com>
2020-06-07 16:36:04 +0300

    Added a summary function and switched to switch
    - Added a new summary function that will summarize URLs using smmry.com
    - Switched to switch, no more if else

--- index.js
+++ index.js
@@ -8,108 +8,143 @@ const functions = require('./functions.js');
 const trivia = require("./trivia.js");
 const reddit = require("./reddit.js");
 const music = require("./music.js");
+const summarize = require("./smmry.js");
 
 //Configs
 const token = config.discord;
 
 bot.on('ready', () => {
-	console.log("Bot is up and running!");
-	bot.user.setActivity('!help');
+    console.log("Bot is up and running!");
+    bot.user.setActivity('!help');
 });
 
 bot.on('voiceStateUpdate', (oldMember, newMember) => {
-	if(newMember.user == bot.user) {
-		if(newMember.voiceChannel == null)
-			music.closeChannel();
-		if(newMember.voiceChannel != null && oldMember.voiceChannel != null)
-			music.setChannel(newMember.voiceChannel);
-		if(newMember.voiceChannel != null)
-			music.setBot(bot.user);
-	}
+    if (newMember.user == bot.user) {
+        if (newMember.voiceChannel == null)
+            music.closeChannel();
+        if (newMember.voiceChannel != null && oldMember.voiceChannel != null)
+            music.setChannel(newMember.voiceChannel);
+        if (newMember.voiceChannel != null)
+            music.setBot(bot.user);
+    }
 });
 
 bot.on('message', message => {
-	if(message.author.bot) return;
-	
-	if(message.content.indexOf('!') === 0) {
-		var text = message.content.substring(1);
-		var words = text.split(" ");
+    if (message.author.bot) return;
 
-		if(words[0] == "help") {
-			functions.sendHelpMessage(message.author);
-			message.delete();
-		} else if(words[0] == "quote") {
-			if(words[1] != null && words[2] != null && message.mentions.everyone == false && message.mentions.users.first() != null) {
-				functions.quoteMessage(message.channel, message.mentions.users.first(), words);
-			} else {
-				message.reply("Usage: !quote [@User] [Message]");
-			}
-		} else if(words[0] == "cat") {
-			functions.requestImage("cat", message);
-		} else if(words[0] == "dog") {
-			functions.requestImage("dog", message);
-		} else if(words[0] == "team") {
-			functions.getRandomTeam(message);
-		} else if(words[0] == "meme") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "4chan") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "blacktwitter") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "dank") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "facepalm") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "meirl") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "memeeconomy") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "wholesome") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "deep") {
-			reddit.send(message, "im14andthisisdeep");
-		} else if(words[0] == "niceguys") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "nicegirls") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "leaguememe") {
-			reddit.send(message, "leagueofmemes");
-		} else if(words[0] == "lwiay") {
-			reddit.send(message, "PewdiepieSubmissions");
-		} else if(words[0] == "loop") {
-			reddit.send(message, "perfectloops");
-		} else if(words[0] == "hmmm") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "woooosh") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "pcmr") {
-			reddit.send(message, "pcmasterrace");
-		} else if(words[0] == "fellowkids") {
-			reddit.send(message, words[0]);
-		} else if(words[0] == "trivia") {
-			trivia.getTrivia(message);
-		} else if(words[0] == "answer") {
-			trivia.answer(message, words);
-		} else if(words[0] == "join") {
-			music.join(message);
-		} else if(words[0] == "volume") {
-			music.volume(message, words);
-		} else if(words[0] == "leave") {
-			music.leave(message);
-		} else if(words[0] == "clear") {
-			music.clearQueue(message);
-		} else if(words[0] == "queue") {
-			music.queue(message, words);
-		} else if(words[0] == "skip") {
-			music.skip(message);
-		} else if(words[0] == "play") {
-			music.play(message, words);
-		}
-		else {
-			message.author.send("Invalid command, please use !help in a chat room to see my commands!");
-			message.delete();
-		}
-	}
+    if (message.content.indexOf('!') === 0) {
+        var text = message.content.substring(1);
+        var words = text.split(" ");
+
+        switch (words[0]) {
+            case "help":
+                functions.sendHelpMessage(message.author);
+                message.delete();
+                break;
+            case "quote":
+                if (words[1] != null && words[2] != null && message.mentions.everyone == false && message.mentions.users.first() != null) {
+                    functions.quoteMessage(message.channel, message.mentions.users.first(), words);
+                } else {
+                    message.reply("Usage: !quote [@User] [Message]");
+                }
+                break;
+            case "cat":
+                functions.requestImage("cat", message);
+                break;
+            case "dog":
+                functions.requestImage("dog", message);
+                break;
+            case "team":
+                functions.getRandomTeam(message);
+                break;
+            case "trivia":
+                trivia.getTrivia(message);
+                break;
+            case "answer":
+                trivia.answer(message, words);
+                break;
+            case "join":
+                music.join(message);
+                break;
+            case "volume":
+                music.volume(message, words);
+                break;
+            case "leave":
+                music.leave(message);
+                break;
+            case "clear":
+                music.clearQueue(message);
+                break;
+            case "queue":
+                music.queue(message, words);
+                break;
+            case "skip":
+                music.skip(message);
+                break;
+            case "play":
+                music.play(message, words);
+                break;
+            case "summarize":
+                summarize.get(message, words[1]);
+                message.delete();
+                break;
+            case "summary":
+                summarize.get(message, words[1]);
+                message.delete();
+                break;
+            case "meme":
+                reddit.send(message, words[0]);
+                break;
+            case "4chan":
+                reddit.send(message, words[0]);
+                break;
+            case "dank":
+                reddit.send(message, "dankmemes");
+                break;
+            case "facepalm":
+                reddit.send(message, words[0]);
+                break;
+            case "meirl":
+                reddit.send(message, words[0]);
+                break;
+            case "memeeconomy":
+                reddit.send(message, words[0]);
+                break;
+            case "deep":
+                reddit.send(message, "im14andthisisdeep");
+                break;
+            case "niceguys":
+                reddit.send(message, words[0]);
+                break;
+            case "nicegirls":
+                reddit.send(message, words[0]);
+                break;
+            case "leaguememe":
+                reddit.send(message, "leagueofmemes");
+                break;
+            case "lwiay":
+                reddit.send(message, "pewdiepiesubmissions");
+                break;
+            case "loop":
+                reddit.send(message, "perfectloops");
+                break;
+            case "hmmm":
+                reddit.send(message, words[0]);
+                break;
+            case "woooosh":
+                reddit.send(message, words[0]);
+                break;
+            case "fellowkids":
+                reddit.send(message, words[0]);
+                break;
+            case "pics":
+                reddit.send(message, words[0]);
+                break;
+            default:
+                message.author.send("Invalid command, please use !help in a chat room to see my commands!");
+                message.delete();
+        }
+    }
 });
 
 bot.login(token);
---
+++ smmry.js
@@ -0,0 +1,53 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
+const request = require('request-promise');
+var validUrl = require('valid-url');
+
+
+module.exports = {
+    get: function(message, url) {
+        if (validUrl.isUri(url))
+            summary(url, message, function(embed) {
+                message.channel.send({ embed });
+                console.log(message.author.username + " called summary on: " + url);
+            });
+    }
+};
+
+function summary(url, message, callback) {
+    request("https://api.smmry.com?SM_KEYWORD_COUNT=3&SM_API_KEY=" + config.smmry + "&SM_URL=" + url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var json = JSON.parse(body);
+            var embed;
+
+            if (json != null) {
+                var keywords = json.sm_api_keyword_array;
+                keywords = keywords.join(", ");
+                embed = new Discord.RichEmbed()
+                    .setTitle(json.sm_api_title > 256 ? json.sm_api_title.slice(0, 256) : json.sm_api_title)
+                    .setAuthor(message.author.username, message.author.avatarURL)
+                    .setURL(url)
+                    .setFooter("Reduced character count: " + json.sm_api_character_count + " | Reduced: " + json.sm_api_content_reduced + " | Keywords: " + keywords);
+
+                var text = json.sm_api_content;
+                text = text.split(". ");
+
+                for (let i = 0; i < text.length - 1; i++) {
+                    embed.addField("\u200b", text[i] + ".", false);
+                }
+
+                callback(embed);
+            } else { //handle error
+                embed = new Discord.RichEmbed()
+                    .setTitle("Error")
+                    .addField("Something bad happend", "An error occured!", true);
+
+                callback(embed);
+            }
+        } else {
+            embed = new Discord.RichEmbed()
+                .setTitle("ERROR")
+                .addField("ERROR", response.statusCode, true);
+        } //handle error
+    });
+}
